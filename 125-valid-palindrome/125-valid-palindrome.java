97af0dfefb89a7768441a48c690f097ccabd2c89
YashPrime-02 <108348585+YashPrime-02@users.noreply.github.com>
2022-07-05 20:24:50 +0530

    Time: 4 ms (88.59%), Space: 44.3 MB (43.16%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,35 +1,16 @@
 class Solution {
-			public boolean isPalindrome(String s) {
-				if(s.length()==0)
-					return true;
-				StringBuilder sb=new StringBuilder();
-				s=s.toLowerCase();
-				for(int i=0;i<s.length();i++)
-				{
-					char ch=s.charAt(i);
-					if(Character.isLetterOrDigit(ch))
-					{
-						sb.append(ch);
-					}
-				}
-				return isPallindrome(sb);
-			}
-			public boolean isPallindrome(StringBuilder sb)
-			{
-				int i=0;
-				int j=sb.length()-1;
-				while(i<=j)
-				{
-					if(sb.charAt(i)==sb.charAt(j))
-					{
-						i++;
-						j--;
-					}
-					else
-					{
-						return false;
-					}
-				}
-				return true;
-			}
+			  public boolean isPalindrome(String s) {
+        StringBuilder clean = new StringBuilder("");
+        s = s.trim();
+        for(int i=0; i< s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch >= 65 && ch <= 90){
+                clean.append((char)(ch+32));
+            }
+            else if((ch >= 97 && ch <= 122) || (ch >=48 && ch<=57) ){
+                clean.append(ch);
+            }
+        }
+        return (clean.toString()).equals(clean.reverse().toString());
+    }
 		}
