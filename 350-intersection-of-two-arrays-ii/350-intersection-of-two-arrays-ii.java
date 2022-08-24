0319d46625e6fc0238a1b930add065566a17e442
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-08-24 18:51:59 +0530

    Time: 4 ms (72.60%), Space: 44 MB (45.54%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -14,16 +14,23 @@ else
 map.put(val,1);
 }
 ArrayList<Integer> list = new ArrayList<>();
-        for(int val : nums2){
-            if(map.get(val) != null && map.get(val) > 0){
-                list.add(val);
-                map.put(val, map.get(val) - 1);
-            }
-        }
-        int[] ret = new int[list.size()]; 
-        for(int i = 0; i < list.size();i++){
-            ret[i] = list.get(i);
-        }
-        return ret;
-    }
+for(int val : nums2)
+{
+if(map.get(val) != null && map.get(val) > 0)
+{
+list.add(val);
+
+int of =map.get(val);
+int nf=of-1;
+map.put(val,nf);
+}
+
+}
+int[] ret = new int[list.size()]; 
+for(int i = 0; i < list.size();i++)
+{
+ret[i] = list.get(i);
+}
+return ret;
+}
 }
