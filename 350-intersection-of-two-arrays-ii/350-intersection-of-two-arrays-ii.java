cffaca96e27cc7918e5e02075f93e944553413c6
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-08-24 18:49:57 +0530

    Time: 3 ms (87.49%), Space: 45.3 MB (5.74%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -1,15 +1,23 @@
 class Solution {
-   public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i : nums1){
-            int freq = map.getOrDefault(i, 0);
-            map.put(i, freq + 1);
-        }
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i : nums2){
-            if(map.get(i) != null && map.get(i) > 0){
-                list.add(i);
-                map.put(i, map.get(i) - 1);
+   public int[] intersect(int[] nums1, int[] nums2) 
+   {
+HashMap<Integer, Integer> map = new HashMap<>();
+for(int val : nums1)
+{
+if(map.containsKey(val))
+{
+int of =map.get(val);
+int nf=of+1;
+map.put(val,nf);
+}
+else
+map.put(val,1);
+}
+ArrayList<Integer> list = new ArrayList<>();
+        for(int val : nums2){
+            if(map.get(val) != null && map.get(val) > 0){
+                list.add(val);
+                map.put(val, map.get(val) - 1);
             }
         }
         int[] ret = new int[list.size()]; 
