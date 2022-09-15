5adefa877ae5db6067988bdec967805c5cbbad01
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-09-15 23:48:11 +0530

    Time: 143 ms (71.71%), Space: 61.5 MB (90.27%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,26 +1,17 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
         
-        // if we don't have an even length array then this is not valie
+     
         if (changed.length % 2 != 0) return new int[]{};
-        
-        // sort the array in ascending order
         Arrays.sort(changed);
-        
-        // create a hashmap that contains each value in the input array 
-        // and its corresponding frequency 
         HashMap<Integer, Integer> freqMap = new HashMap();
         for(int i = 0; i < changed.length; i++) {
             int curr = changed[i];
             freqMap.put(curr, freqMap.getOrDefault(curr, 0) + 1);
         }
-    
-        // the final array should be half the size of the input array
-        int[] result = new int[changed.length / 2];
+       int[] result = new int[changed.length / 2];
         int index = 0;
         for(int i: changed) {
-            // edge case: if the nubmer is 0, then the double is also 0
-            //and we must decrement by 2
             if (i == 0 && freqMap.containsKey(0) && freqMap.get(0) >= 2) {
                 decrementValue(freqMap, 0, 2);
                 result[index] = 0;
@@ -28,7 +19,7 @@ class Solution {
             } else if (i != 0 
                        && freqMap.containsKey(i) 
                        && freqMap.containsKey(i * 2)) {
-                // if a value and its double is in the map, then decrement both values
+               
                 decrementValue(freqMap, i, 1);
                 decrementValue(freqMap, i * 2, 1);
                 result[index] = i;
