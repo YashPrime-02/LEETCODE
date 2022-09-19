2cc286e3da4ed9513933260e340dd80de3fc2d4a
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-09-19 12:30:20 +0530

    Time: 118 ms (67.22%), Space: 123.4 MB (80.45%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int[] findOriginalArray(int[] changed) {
-    int[] temp = new int[changed.length/2];
+    public int[] findOriginalArray(int[] multi) {
+    int[] temp = new int[multi.length/2];
     Queue<Integer> x = new LinkedList<>();
-    Arrays.sort(changed);
+    Arrays.sort(multi);
     int i = 0;
-    for(int num : changed){
+    for(int num : multi){
         if(!x.isEmpty() && x.peek()==num)
             temp[i++] = x.poll()/2;
         else x.add(num*2);
