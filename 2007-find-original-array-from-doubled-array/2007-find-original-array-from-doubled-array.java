ef89d1260683769e3bca4fbebff5ca8f588f0696
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-09-19 12:29:35 +0530

    Time: 131 ms (63.04%), Space: 122.2 MB (83.24%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,43 +1,14 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
-        
-     
-        if (changed.length % 2 != 0) return new int[]{};
-        Arrays.sort(changed);
-        HashMap<Integer, Integer> freqMap = new HashMap();
-        for(int i = 0; i < changed.length; i++) {
-            int curr = changed[i];
-            freqMap.put(curr, freqMap.getOrDefault(curr, 0) + 1);
-        }
-       int[] result = new int[changed.length / 2];
-        int index = 0;
-        for(int i: changed) {
-            if (i == 0 && freqMap.containsKey(0) && freqMap.get(0) >= 2) {
-                decrementValue(freqMap, 0, 2);
-                result[index] = 0;
-                index++;
-            } else if (i != 0 
-                       && freqMap.containsKey(i) 
-                       && freqMap.containsKey(i * 2)) {
-               
-                decrementValue(freqMap, i, 1);
-                decrementValue(freqMap, i * 2, 1);
-                result[index] = i;
-                index++;
-            } 
-        }
-        
-        if (freqMap.size() > 0) return new int[]{};
-        
-        return result;
+    int[] temp = new int[changed.length/2];
+    Queue<Integer> x = new LinkedList<>();
+    Arrays.sort(changed);
+    int i = 0;
+    for(int num : changed){
+        if(!x.isEmpty() && x.peek()==num)
+            temp[i++] = x.poll()/2;
+        else x.add(num*2);
     }
-    
-    // decrments the frequency of a particular key in the hashmap.  If we end up at 0 frequency,
-    // we will remvove the key from the map
-    public void decrementValue(HashMap<Integer, Integer> freqMap, int value, int amount) {
-        freqMap.put(value, freqMap.get(value) - amount);
-        if (freqMap.get(value) == 0) {
-            freqMap.remove(value);
-        }
-    }
+    return x.size()>0 ? new int[]{}:temp;
 }
+    }
