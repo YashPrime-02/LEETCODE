ade2c8ca1c30fdd0dd4d1e1d40cbdb075c3afc8f
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-09-21 20:50:41 +0530

    Time: 16 ms (11.48%), Space: 54 MB (60.44%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.java
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.java
@@ -4,7 +4,7 @@ class Solution {
         int result= 0;
         for(int i=0;i<nums.length-2;i++){
             int a=nums[i],b=nums[i+1],c=nums[i+2];            
-            if(a+b>c&b+c>a&&c+a>b)
+            if(a+b>c&&b+c>a&&c+a>b)
                 result= a+b+c;
         }
         return result;
