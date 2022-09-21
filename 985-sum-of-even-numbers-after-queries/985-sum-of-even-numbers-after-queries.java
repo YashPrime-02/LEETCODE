c5be37e5792b0044cf0f87e4db53311dccb495c4
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-09-21 14:29:39 +0530

    Time: 4 ms (99.68%), Space: 50.3 MB (95.50%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -1,24 +1,30 @@
 class Solution {
-    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
-    int evenSum=0;
-    int[] ans = new int[queries.length];
-    int k=0;
-    for(int i=0;i<nums.length;i++){
-        if(nums[i]%2==0){
-            evenSum+=nums[i];
+    public int[] sumEvenAfterQueries(int[] nums, int[][] q) {
+        int l = q.length,firstNum = 0;
+        int [] ans = new int[l];
+        for(int a:nums){
+            if(a%2==0)firstNum+=a;
         }
+        for(int i=0;i<l;i++){
+            int index = q[i][1] , val = q[i][0] ,a = nums[index];
+            if(a%2==0){
+                if((a + val)%2==0){
+                    ans[i] = firstNum + val;
+                    firstNum = firstNum + val;
+                }else{
+                    ans[i] = firstNum - a;
+                    firstNum = firstNum - a;
+                }
+            }else{
+                if((a + val)%2==0){
+                    ans[i] = firstNum + val + a;
+                    firstNum = firstNum + val + a;
+                }else{
+                    ans[i] = firstNum;
+                }
+            }
+            nums[index] = nums[index] + val;
+        }
+        return ans;
     }
-    for(int[] i : queries){
-        if(nums[i[1]]%2==0){
-            evenSum-=nums[i[1]];
-        }
-        nums[i[1]]+=i[0];
-        if(nums[i[1]]%2==0){
-            evenSum+=nums[i[1]];
-        }
-        ans[k++]=evenSum;
-    }
-    return ans;
-    
-}
 }
