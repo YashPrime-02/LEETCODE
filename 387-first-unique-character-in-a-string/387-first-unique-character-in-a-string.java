5da0a623ab91dcf7130d3d6e187a2b1fbd201194
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-09-24 16:43:48 +0530

    Time: 21 ms (58.40%), Space: 42.5 MB (98.04%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,11 +1,18 @@
-public class Solution {
-    public int firstUniqChar(String s) {
-        int freq [] = new int[26];
-        for(int i = 0; i < s.length(); i ++)
-            freq [s.charAt(i) - 'a'] ++;
-        for(int i = 0; i < s.length(); i ++)
-            if(freq [s.charAt(i) - 'a'] == 1)
-                return i;
-        return -1;
+class Solution {
+    public int firstUniqChar(String str) {
+        HashMap <Character,Integer> mp= new LinkedHashMap<Character,Integer>();
+        HashMap <Character,Integer> m1= new LinkedHashMap<Character,Integer>();
+        for(int i=0; i<str.length(); i++){
+            if(!m1.containsKey(str.charAt(i))){
+                mp.put(str.charAt(i), i);
+                m1.put(str.charAt(i), i);
+            }
+            else{
+                if(mp.containsKey(str.charAt(i))) mp.remove(str.charAt(i));
+            }
+        }
+        if(mp.size()==0) return -1;
+        Object x= mp.keySet().toArray()[0];
+        return mp.get((char)x);
     }
 }
