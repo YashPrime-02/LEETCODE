929bbbe80a9afc73dd38d6674a8d022cdd3eb479
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-09-26 00:24:13 +0530

    Time: 11 ms (15.69%), Space: 48.2 MB (49.63%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.java
+++ 622-design-circular-queue/622-design-circular-queue.java
@@ -1,31 +1,77 @@
 class MyCircularQueue {
-        final int[] a;
-        int front = 0, rear = -1, len = 0;
-
-        public MyCircularQueue(int k) { a = new int[k];}
-
-        public boolean enQueue(int val) {
-            if (!isFull()) {
-                rear = (rear + 1) % a.length;
-                a[rear] = val;
-                len++;
-                return true;
-            } else return false;
-        }
-
-        public boolean deQueue() {
-            if (!isEmpty()) {
-                front = (front + 1) % a.length;
-                len--;
-                return true;
-            } else return false;
-        }
-
-        public int Front() { return isEmpty() ? -1 : a[front];}
-
-        public int Rear() {return isEmpty() ? -1 : a[rear];}
-
-        public boolean isEmpty() { return len == 0;}
-
-        public boolean isFull() { return len == a.length;}
+int queue[];
+int front,rear,size;       
+    
+    public MyCircularQueue(int k) 
+    {
+    this.queue=new int[k]; 
+    this.front=-1;
+    this.rear=-1;
+    this.size=k;
     }
+    
+    public boolean enQueue(int value) 
+    {
+    if(isFull())
+       return(false);
+            
+    if(isEmpty())
+    {
+    front=rear=0;
+            
+    }
+     else
+    {
+    rear=(rear+1)%size;        
+    }
+    queue[rear]=value;        
+    return true;
+    }
+    
+    public boolean deQueue() 
+    {
+     if(isEmpty())
+     return(false);
+     
+     if(front == rear){
+        front = rear = -1;
+     }       
+     else
+     {
+     front=(front+1)%size;
+     }
+     return true;       
+            
+    }
+    
+    public int Front() 
+    {
+     return((isEmpty())?-1:queue[front]);   
+    }
+    
+    public int Rear() 
+    {
+    return((isEmpty())?-1:queue[rear]);    
+    }
+    
+    public boolean isEmpty() 
+    {
+    return(front==-1);    
+    }
+    
+    public boolean isFull() 
+    {
+    return((rear + 1) % size ==(front));    
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
