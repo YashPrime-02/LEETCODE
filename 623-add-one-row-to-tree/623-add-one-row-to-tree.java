491a7c11d11d192118a6f5d1e139a1eeee1b2554
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2022-10-06 19:38:10 +0530

    Time: 1 ms (88.29%), Space: 46 MB (10.87%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.java
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.java
@@ -1,15 +1,40 @@
 public class Solution {
     public TreeNode addOneRow(TreeNode root, int v, int d) {
-        if (d == 0 || d == 1) {
-            TreeNode newroot = new TreeNode(v);
-            newroot.left = d == 1 ? root : null;
-            newroot.right = d == 0 ? root : null;
-            return newroot;
-        }
-        if (root != null && d >= 2) {
-            root.left  = addOneRow(root.left,  v, d > 2 ? d - 1 : 1);
-            root.right = addOneRow(root.right, v, d > 2 ? d - 1 : 0);
-        }
-        return root;
+       
+            if (d == 1){
+                    TreeNode newroot = new TreeNode(v);
+                    newroot.left = root;
+                    return newroot;
+            }
+            
+            helper(root, v, 1, d);
+             return root;
     }
+
+        void helper(TreeNode root, int val, int d, int depth){
+                
+                if(root == null){
+                return;
+                }
+                
+                if (d < depth){
+                        
+                        helper(root.left, val, d+1, depth);
+                        helper(root.right, val, d+1, depth);
+                }
+                
+                if (d == depth-1){
+                        
+                        TreeNode left = new TreeNode(val);
+                        TreeNode right = new TreeNode(val);
+                        
+                        left.left = root.left; 
+                        right.right = root.right;
+                        
+                        root.left = left;
+                        root.right = right;
+                }
+                
+                
+        }
 }
